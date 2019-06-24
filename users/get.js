bb6558e3899e9e038b707a0d888cd0660d7d79fc
J. Federico Hernandez <fede.hernandez@gmail.com>
2019-06-23 23:51:27 -0300

    Implemented user read endpoint.

--- users/get.js
+++ users/get.js
@@ -1,9 +1,37 @@
 'use strict';
 
+const dynamodb = require('./dynamodb');
+
 module.exports.get = async (event, context) => {
+    console.log('--- id: ', event.pathParameters.id);
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            id: event.pathParameters.id,
+        },
+    };
+
+    // fetch todo from the database
+    try {
+        const result = await dynamodb.get(params).promise();
+        // create a response
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t fetch the todo item.',
+        };
+    }
+
     // create a response
     return {
-        statusCode: 200,
-        body: event.pathParameters.id,
+        statusCode: 400,
+    // body: JSON.stringify(result.Item),
     };
 };
