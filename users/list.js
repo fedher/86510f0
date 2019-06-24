c7df263f163f53fcdece0fae6302b4a391b35128
J. Federico Hernandez <fede.hernandez@gmail.com>
2019-06-23 23:51:47 -0300

    Implemented user list endpoint.

--- users/list.js
+++ users/list.js
@@ -1,9 +1,28 @@
 'use strict';
 
-module.exports.get = async (event, context) => {
-    // create a response
-    return {
-        statusCode: 200,
-        body: 'list',
+const dynamodb = require('./dynamodb');
+
+module.exports.list = async (event, context) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
     };
+
+    // fetch all todos from the database
+    try {
+        const result = await dynamodb.scan(params).promise();
+        // create a response
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Items),
+        };
+    } catch (error) {
+    // handle potential errors
+        console.error(error);
+        return {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t fetch the todo item.',
+        };
+    }
+
 };
