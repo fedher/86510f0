f775b0e2207d7c7e1ec964818cf1c9cba9b07ed6
J. Federico Hernandez <fede.hernandez@gmail.com>
2019-06-23 23:47:44 -0300

    Added user schema.

---
+++ users/delete.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const dynamodb = require('./dynamodb');
+
+module.exports.delete = async (event, context) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            id: event.pathParameters.id,
+        },
+    };
+
+    // delete the todo from the database
+    try {
+        const result = await dynamodb.delete(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({}),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t remove the todo item.',
+        };
+    }
+};
