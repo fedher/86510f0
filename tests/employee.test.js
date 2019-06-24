1fe69610393aca3fce0deba86699af970f2713f6
J. Federico Hernandez <fede.hernandez@gmail.com>
2019-06-24 12:17:31 -0300

    Added employee test for list endpoint.

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -5,8 +5,8 @@ const apiKey = require('./keys/employee');
 
 const url = 'http://localhost:3000/users';
 
-let user1Id;
-let user2Id;
+let userIdA;
+let userIdB;
 
 describe('Employee Tests:', () => {
 
@@ -18,7 +18,7 @@ describe('Employee Tests:', () => {
                 password: 'test234',
                 role: 'employee'
             });
-            user1Id = body.id;
+            userIdA = body.id;
 
             assert.equal(status, 200, 'status should be 200');
             assert.equal(body.name, 'Carl Pulkit');
@@ -38,7 +38,7 @@ describe('Employee Tests:', () => {
                 password: 'test',
                 role: 'employee'
             });
-            user2Id = body.id;
+            userIdB = body.id;
 
             assert.equal(status, 200, 'status should be 200');
             assert.equal(body.name, 'Juan Mon');
@@ -52,7 +52,7 @@ describe('Employee Tests:', () => {
 
     it('should not get an employee data as another emplyee', async () => {
         try {
-            await rest.req('get', `${url}/${user2Id}`, apiKey);
+            await rest.req('get', `${url}/${userIdB}`, apiKey);
             assert.fail();
         } catch (error) {
             assert.equal(error.statusCode, 403, 'status should be 403');
@@ -61,7 +61,7 @@ describe('Employee Tests:', () => {
 
     it('should update their personal user data', async () => {
         try {
-            const { body, status } = await rest.req('put', `${url}/${user1Id}`, apiKey, {
+            const { body, status } = await rest.req('put', `${url}/${userIdA}`, apiKey, {
                 name: 'Pity Martinez',
                 email: 'pity@m.com',
                 password: 'test2'
@@ -79,7 +79,7 @@ describe('Employee Tests:', () => {
 
     it('should not update other user data', async () => {
         try {
-            await rest.req('put', `${url}/${user2Id}`, apiKey, {
+            await rest.req('put', `${url}/${userIdB}`, apiKey, {
                 name: 'Pity Martinez',
                 email: 'pity@m.com',
                 password: 'test2'
@@ -94,7 +94,7 @@ describe('Employee Tests:', () => {
 
     it('should not delete other employee data', async () => {
         try {
-            await rest.req('delete', `${url}/${user2Id}`, apiKey);
+            await rest.req('delete', `${url}/${userIdB}`, apiKey);
             assert.fail();
 
         } catch (error) {
@@ -110,7 +110,7 @@ describe('Employee Tests:', () => {
                 password: 'test234',
                 role: 'employee'
             });
-            user1Id = body.id;
+            userIdA = body.id;
 
             assert.equal(status, 200, 'status should be 200');
             assert.equal(body.name, 'Carl Pulkit');
@@ -122,9 +122,19 @@ describe('Employee Tests:', () => {
         }
     });
 
+    it('should not get a list of employees as employee', async () => {
+        try {
+            await rest.req('get', `${url}`, apiKey);
+            assert.fail();
+
+        } catch (error) {
+            assert.equal(error.statusCode, 403, 'status should be 403');
+        }
+    });
+
     it('should delete their own user data', async () => {
         try {
-            const { status } = await rest.req('delete', `${url}/${user1Id}`, apiKey);
+            const { status } = await rest.req('delete', `${url}/${userIdA}`, apiKey);
             assert.equal(status, 200, 'status should be 200');
 
         } catch (error) {
